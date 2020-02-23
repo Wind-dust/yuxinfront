257ef9257a11d00479817ac64ff0b47cbb9c3a37
jackiwu <917436062@qq.com>
2020-02-23 21:09:37 +0800

    '列表'

--- src/components/page/report/sendDetail.vue
+++ src/components/page/report/sendDetail.vue
@@ -7,13 +7,19 @@
           <div v-if="status">
             <h3 class="title">任务列表</h3>
             <el-select style="width: 150px" size="small" v-model="type" placeholder="请选择产品" @change="query(type)">
-              <el-option v-for="(v,k) in service" :key="k" :label="v.business_name" :value="v.business_id" ></el-option>
+              <el-option v-for="(v,k) in service" :key="k" :label="v.business_name" :value="v.business_id"></el-option>
             </el-select>
             <!--<el-button size="mini" type="primary" @click="query(type)">查询</el-button>-->
             <el-table ref="multipleTable" :data="list" tooltip-effect="dark" style="width: 100%">
               <!--<el-table-column type="selection"></el-table-column>-->
-              <el-table-column prop="task_name" show-overflow-tooltip label="名称"></el-table-column>
-              <el-table-column prop="task_content" show-overflow-tooltip label="内容"></el-table-column>
+              <el-table-column prop="task_name" show-overflow-tooltip label="名称" v-if="type == 5"></el-table-column>
+              <el-table-column :label="type == 8?'标题':'内容'">
+                <template slot-scope="scope">
+                  <span v-if="type == 8">{{scope.row.title}} <el-button type="text"
+                                                                        @click="preview(scope.row.multimedia_frame,scope.row.title)">查看内容</el-button></span>
+                  <span v-else>{{scope.row.task_content}}</span>
+                </template>
+              </el-table-column>
               <el-table-column prop="mobile_content" show-overflow-tooltip label="手机号"></el-table-column>
               <el-table-column prop="send_num" label="发送数量"></el-table-column>
               <el-table-column prop="source" label="来源" show-overflow-tooltip></el-table-column>
@@ -24,7 +30,8 @@
                 </template>
               </el-table-column>
             </el-table>
-            <v-pagination  @pageChange="pageChange" :num='num' :total="total" :page-size="10" :page="page"></v-pagination>
+            <v-pagination @pageChange="pageChange" :num='num' :total="total" :page-size="10"
+                          :page="page"></v-pagination>
           </div>
           <div v-else>
             <h3 class="title">明细查询</h3>
@@ -38,8 +45,8 @@
                 <!--<el-table-column prop="send_num" label="条数"></el-table-column>-->
                 <el-table-column prop="create_time" label="提交时间" show-overflow-tooltip></el-table-column>
                 <!--<el-table-column prop="create_time" label="回执时间" show-overflow-tooltip></el-table-column>-->
-                <el-table-column prop="_send_status" label="状态"  show-overflow-tooltip></el-table-column>
-                <el-table-column prop="explanation" label="回执状态"  show-overflow-tooltip></el-table-column>
+                <el-table-column prop="_send_status" label="发送状态" show-overflow-tooltip></el-table-column>
+                <el-table-column prop="explanation" label="回执状态" show-overflow-tooltip></el-table-column>
               </el-table>
               <v-pagination @pageChange="pageChangeDetail" :num='detailNum' :total="detailTotal" :page-size="10"
                             :page="detailPage"></v-pagination>
@@ -48,6 +55,17 @@
         </div>
       </el-col>
     </el-row>
+
+    <el-dialog title="详细内容" :visible.sync="dialogVisible" width="30%">
+      <p style="color: #848a9f;font-weight: bold;margin-bottom: 20px">{{title}}</p>
+      <div v-for="v,k in content">
+        <img style="object-fit: cover;max-width: 80%;" :src="v.image_path" alt="">
+        <p style="margin: 10px 0;max-width: 80%;">{{v.content}}</p>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">关 闭</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -65,13 +83,14 @@
       return {
         status: true,
         list: [],
+        content:[],
         screen: {
           page: 1,
           pageNum: 10
         },
-        codeScreen:{
-          page:1,
-          pageNum:10
+        codeScreen: {
+          page: 1,
+          pageNum: 10
         },
         page: 1,
         total: 0,
@@ -79,16 +98,18 @@
         num: 1,
         task: {},
         zan: true,
-        task_log:[],
-        detailNum:1,
-        detailTotal:20,
-        detailPage:1,
-        detailScreen:{
-          page:1,
-          pageNum:10
+        task_log: [],
+        detailNum: 1,
+        detailTotal: 20,
+        detailPage: 1,
+        detailScreen: {
+          page: 1,
+          pageNum: 10
         },
-        service:[],
-        type:''
+        service: [],
+        type: '',
+        dialogVisible:false,
+        title:''
       }
     },
     mounted() {
@@ -100,7 +121,13 @@
       this.getService()
     },
     methods: {
-      query(type){
+      preview(data,title) {
+        console.log(data)
+        this.content = data
+        this.title = title
+        this.dialogVisible = true
+      },
+      query(type) {
         this.screen.page = 1
         this.screen.pageNum = 10
         this.num = 1
@@ -108,13 +135,27 @@
         this.page = 1
         this.list = []
         console.log(this.type)
-        if (this.type == 5){
+        if (this.type == 5) {
           this.getTaskList()
         } else if (this.type == 6) {
           this.getCodeTask()
+        } else if (this.type == 8) {
+          this.getMmsTask()
         }
       },
-      getCodeTask(){
+      getMmsTask() {
+        let that = this
+        let screen = that.screen
+        that.$request({
+          url: 'user/getUserMultimediaMessageTask',
+          data: screen,
+          success(res) {
+            that.list = res.message_data
+            that.total = res.total
+          }
+        })
+      },
+      getCodeTask() {
         let that = this
         let screen = that.screen
         that.$request({
@@ -141,13 +182,13 @@
       disService(data) {
         for (let i = 0; i < data.length; i++) {
           this.ruleType.business_id.option.push({
-            value:data[i].business_id,
-            label:data[i].business_name
+            value: data[i].business_id,
+            label: data[i].business_name
           })
         }
       },
-      emit(){
-        this.$emit('getBread','明细查询')
+      emit() {
+        this.$emit('getBread', '明细查询')
       },
       shouqi() {
         console.log(!this.zan)
@@ -163,17 +204,30 @@
         this.detailTotal = 0
         this.detailScreen.pageNum = 10
         this.detailScreen.page = 1
-        if (this.type == 5){
+        if (this.type == 5) {
           this.getTaskInfoDetail(id)
-        } else if(this.type == 6){
+        } else if (this.type == 6) {
           this.getCodeTaskDetail(id)
+        } else if (this.type == 8){
+          this.getMmsTasDetailk(id)
         }
       },
-      getCodeTaskDetail(id){
+      getMmsTasDetailk(id){
+        let that = this
+        that.$request({
+          url:'user/getUserMultimediaMessageTaskInfo',
+          data:{id:id,pageNum: that.detailScreen.pageNum, page: that.detailScreen.page},
+          success(res){
+            that.task_log = that.disinfo(res.task_log)
+            that.detailTotal = res.total
+          }
+        })
+      },
+      getCodeTaskDetail(id) {
         let that = this
         that.$request({
           url: 'user/getUserBusinessSubmitTaskInfo',
-          data: {id:id,pageNum:that.detailScreen.pageNum,page:that.detailScreen.page},
+          data: {id: id, pageNum: that.detailScreen.pageNum, page: that.detailScreen.page},
           success(res) {
             that.task_log = that.disinfo(res.task_log)
             that.detailTotal = res.total
@@ -184,24 +238,24 @@
         let that = this
         that.$request({
           url: 'user/getUserSubmitTaskInfo',
-          data: {id:id,pageNum:that.detailScreen.pageNum,page:that.detailScreen.page},
+          data: {id: id, pageNum: that.detailScreen.pageNum, page: that.detailScreen.page},
           success(res) {
             that.task_log = that.disinfo(res.task_log)
             that.detailTotal = res.total
           }
         })
       },
-      disinfo(data){
+      disinfo(data) {
         console.log(data)
         let _send_status = ''
-        for (let i=0;i<data.length;i++){
-          if (data[i].send_status == 1){
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].send_status == 1) {
             data[i]._send_status = '待发送'
-          } else if (data[i].send_status == 2){
+          } else if (data[i].send_status == 2) {
             data[i]._send_status = '已完成'
-          } else if (data[i].send_status == 3){
+          } else if (data[i].send_status == 3) {
             data[i]._send_status = '成功'
-          }else if (data[i].send_status == 4){
+          } else if (data[i].send_status == 4) {
             data[i]._send_status = '失败'
           }
         }
@@ -211,12 +265,14 @@
         this.screen.page = obj.page
         if (this.type === 5) {
           this.getTaskList()
-        } else if (this.type===6){
+        } else if (this.type === 6) {
           this.getCodeTask()
+        } else if (this.type === 8){
+          this.getMmsTask()
         }
 
       },
-      pageChangeDetail(obj){
+      pageChangeDetail(obj) {
         this.detailScreen.page = obj.page
         localStorage.setItem("task_log", obj.page)
         this.getTaskInfoDetail(this.id)
@@ -263,6 +319,7 @@
     top: 4px;
     left: -10px;
   }
+
   .back {
     display: inline-block;
     float: right;
